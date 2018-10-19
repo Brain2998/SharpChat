e14e0e872b42c2e88f0f2fd10dbbc294d65c496d
brain2998 <an.sereb96@gmail.com>
2018-10-19 22:20:06 +0300

    Before server graceful close

---
+++ Connection.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.IO;
+
+namespace SharpChat
+{
+    public class Connection
+    {
+		private TcpClient tcpClient;
+		private Thread clientThread;
+		private StreamReader clientReader;
+		private StreamWriter clientWriter;
+		private string clientResponse;
+		private string clientName;
+
+		public Connection(TcpClient Client)
+        {
+			tcpClient = Client;
+			clientThread = new Thread(AcceptClient);
+			clientThread.Start();
+        }
+
+        public void CloseConnection(string reason)
+		{
+			if (reason != "3")
+			{
+				clientWriter.WriteLine(reason);
+				clientWriter.Flush();
+			}
+			tcpClient.Close();
+			clientThread.Abort();
+			clientReader.Close();
+			clientWriter.Close();
+		}
+
+        private void AcceptClient()
+		{
+			clientReader = new StreamReader(tcpClient.GetStream());
+			clientWriter = new StreamWriter(tcpClient.GetStream());
+			clientName = clientReader.ReadLine();
+			if (clientName != "")
+			{
+				if (Server.UserTable.Contains(clientName))
+				{
+					CloseConnection("2");
+				}
+				else
+				{
+					clientWriter.WriteLine("0");
+					clientWriter.Flush();
+					Server.AddUser(tcpClient, clientName);
+				}
+			}
+			else
+			{
+				CloseConnection("1");
+			}
+			try
+			{
+				while ((clientResponse=clientReader.ReadLine())!="")
+				{
+					if (clientResponse=="3")
+					{
+						Server.RemoveUser(tcpClient, clientName);
+						CloseConnection("3");
+					}
+				}
+			}
+			catch
+			{
+			//	CloseConnection("1");
+			}
+		}      
+    }
+}
--- Server.cs
+++ Server.cs
@@ -1,25 +1,85 @@
 ﻿using System;
 using System.Net;
 using System.Net.Sockets;
+using System.Collections;
+using System.Threading;
+using System.Collections.Generic;
 
 namespace SharpChat
 {
     public class Server
     {
 		private IPAddress ipAddress;
-	    //private TcpClient tcpClient;
+	    private TcpClient tcpClient;
 	    private TcpListener tcpListener;
+		private static Hashtable Users = new Hashtable();
+		private static MainWindow ChatForm;
+		private Thread thrListener;
+		private List<Connection> Connections;
 
-		public Server(IPAddress address)
+		public static Hashtable UserTable
+		{
+			get 
+			{
+				return Users;
+			}
+			set
+			{
+				Users = value;
+			}
+		}
+
+		public Server(IPAddress address, MainWindow form)
 		{
 			ipAddress = address;
+			ChatForm = form;
 		}
 
 		public void StartListening()
 		{
 			tcpListener=new TcpListener(ipAddress, 1986);
 			tcpListener.Start();
-
+			thrListener = new Thread(KeepListening);
+			thrListener.Start();
         }
+
+		private void KeepListening()
+		{
+			while (true)
+            {
+                tcpClient = tcpListener.AcceptTcpClient();
+				if (tcpClient.Connected)
+				{
+					Connection connection = new Connection(tcpClient);
+					//Connections.Add(connection);
+				}
+            }
+		}
+
+		public void StopListening()
+		{
+			tcpListener.Stop();
+			thrListener.Abort();
+			/*foreach (TcpClient client in Users.Values)
+			{
+				client.Close();
+			}*/
+			for (int i = 0; i < Connections.Count; ++i)
+			{
+				Connections[i].CloseConnection("4");
+			}
+		}
+
+		public static void AddUser(TcpClient User, string Username)
+		{
+			Users.Add(Username, User);
+			ChatForm.Log = Username + " join\n";
+		}
+
+		public static void RemoveUser(TcpClient user, string Username)
+		{
+			Users.Remove(Username);
+			ChatForm.Log = Username + " left\n";
+		}
     }
 }
